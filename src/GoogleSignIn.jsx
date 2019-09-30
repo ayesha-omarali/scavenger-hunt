889e14482bf2b63091e8b91f89a5c0127ece4831
Vincent Van Buskirk <vvanbuskirk805@gmail.com>
2019-09-29 22:36:21 -0700

    fix google oauth

--- src/GoogleSignIn.jsx
+++ src/GoogleSignIn.jsx
@@ -4,23 +4,36 @@ import './App.css';
 import styled from '@emotion/styled'
 import { Flex, Box } from '@rebass/grid/emotion'
 import ReactDOM from 'react-dom';
+import { Redirect } from 'react-router-dom'
 import GoogleLogin from 'react-google-login';
 
 export default class GoogleSignIn extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = { loggedIn: false }
+  }
 
   responseGoogle = (response) => {
-    console.log(response);
+    localStorage.setItem('email', response.profileObj.email);
+    localStorage.setItem('loggedIn', true);
+    this.setState({ loggedIn: true });
   }
 
   render() {
-    return (
+    return !this.state.loggedIn ? 
+    (
       <GoogleLogin
       clientId={'347105098552-3jvf06c6oq42qrpslcdk5pp8dor9m9a2.apps.googleusercontent.com'}
       onSuccess={this.responseGoogle}
       onFailure={this.responseGoogle}
       >
         <span> Login with Google</span>
-    </GoogleLogin>
-    );
+    </GoogleLogin> 
+    ) : 
+    (
+      <Redirect to={{
+        pathname: '/game',
+      }}/>
+    )
   }
 }

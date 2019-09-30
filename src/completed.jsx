690e6d4554edbfbf9dde1aaef22d591df963986e
Ayesha Omarali <ayesha.omarali@gmail.com>
2019-09-29 22:31:51 -0700

    added contact component, updated nav bar and mission display tabs

---
+++ src/completed.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import styled from '@emotion/styled'
+import { Flex, Box } from '@rebass/grid/emotion'
+import GameContainer from './GameContainer';
+import Card from 'react-bootstrap/Card';
+
+export default class Completed extends React.Component {
+
+  render() {
+    const totalPoints = 9;
+    const tasks = [
+      {
+        title: 'Starting Task!', 
+        subtitle: '10 points',
+        text: 'Take a selfie with your team to mark the start of your adventures!',
+        points: 10
+      },
+    ]
+
+    return(
+      <CompletedContainer>
+        <TalliedPoints>
+          Total Points: {totalPoints}
+        </TalliedPoints>
+        {tasks.map(TaskCard)}
+      </CompletedContainer>
+    )
+  }
+}
+
+const TaskCard = ({title, subtitle, text}) => {
+  return (
+    <React.Fragment>
+      <Card style={{ width: '100%', borderColor: '#FDB515' }}>
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">{subtitle}</Card.Subtitle>
+          <Card.Text>
+            {text}
+          </Card.Text>
+          <Card.Link href="#">Submit Evidence</Card.Link>
+        </Card.Body>
+      </Card>
+      <p></p>
+    </React.Fragment>
+  )
+}
+
+const CompletedContainer = styled(Box)`
+  flex-grow: 1;
+  background-color: white;
+  justify-content: center;
+  padding: 30px;
+  overflow: scroll;
+  font-family: sans-serif;
+`;
+
+const TalliedPoints = styled(Flex)`
+  font-family: helvetica-bold;
+  font-size: calc(20px + 2vmin);
+  padding: 10px 0px;
+`;
---
+++ src/contact.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from '@emotion/styled'
+import { Flex, Box } from '@rebass/grid/emotion'
+import Card from 'react-bootstrap/Card';
+
+export default class Contact extends React.Component {
+
+  render() {
+    return(
+      <ContactContainer>
+        <Card style={{ width: '100%', borderColor: '#FDB515' }}>
+          <Card.Body>
+            <Card.Title>Need help? Something broke? Want a Friend?</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">Contact tech support!! (aka Ayesha)</Card.Subtitle>
+            <Card.Text>
+              (925) 732 - 8254
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </ContactContainer>
+    )
+  }
+}
+
+const ContactContainer = styled(Box)`
+  flex-grow: 1;
+  background-color: white;
+  justify-content: center;
+  padding: 30px;
+  overflow: scroll;
+  font-family: sans-serif;
+`;
